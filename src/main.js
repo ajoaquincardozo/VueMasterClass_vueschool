0615691c2c3267c7d23b35183fea6454e8046c96
ajoaquincardozo <a.joaquincardozo@gmail.com>
2020-08-16 18:53:38 -0300

    [VueMasterClass]: firebase + lifecycle hooks -Se agrega firebase a proyecto -Se importa data.json, con la particularidad de que para realizar esto se tuvo que borrar la property .key -Se agrega en PageHome, metodos que explican el lifeCycle Hooks de Vue.

--- src/main.js
+++ src/main.js
@@ -3,11 +3,26 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import firebase from 'firebase/app'
 import store from './store'
 import AppDate from '@/components/AppDate'
 
 Vue.component('AppDate', AppDate)
 Vue.config.productionTip = false
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyDB4iWeBq40NRl6MeH7qdJobiZTwVJ26n8',
+  authDomain: 'vue-school-forum-11bc5.firebaseapp.com',
+  databaseURL: 'https://vue-school-forum-11bc5.firebaseio.com',
+  projectId: 'vue-school-forum-11bc5',
+  storageBucket: 'vue-school-forum-11bc5.appspot.com',
+  messagingSenderId: '579271514916',
+  appId: '1:579271514916:web:42709066b8b2069de5f1b1'
+}
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig)
 
 /* eslint-disable no-new */
 new Vue({
