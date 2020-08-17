a009f090485fabd91c39a612144fea0b2f1216b7
ajoaquincardozo <a.joaquincardozo@gmail.com>
2020-08-16 21:17:50 -0300

    [VueMasterClass]: firebase + Vuex(Best-practices) -Se aplica refactor sobre implementacion de firebase que trae thread/posts/user/etc, ya que no se deberian hacer mutaciones en la misma definicion del componente sino a traves de actions.   --Se lleva implementacion a store/index.js por medio de impl de actions   --Las actions implementadas devuelven promesan con el objeto obtenido a partir del state (re-validacion)   --Se cambia impl en componente para usar las acciones por medio del store.dispatch(methodName, payLoad)

--- src/pages/PageThreadShow.vue
+++ src/pages/PageThreadShow.vue
@@ -25,8 +25,6 @@
 </template>
 
 <script>
-    import firebase from 'firebase/app'
-    import 'firebase/database'
     import PostList from '@/components/PostList'
     import PostEditor from '@/components/PostEditor'
     import { countObjectProperties } from '@/utils'
@@ -73,27 +71,15 @@
         // Se puede pensar como una llamada AJAX sin el costo de una llamada Http.
 
         // fetch thread
-        firebase.database().ref('threads').child(this.id).once('value', snapshot => {
-          const thread = snapshot.val()
-          this.$store.commit('setThread', { threadId: snapshot.key, thread: { ...thread, '.key': snapshot.key } })
-
+        this.$store.dispatch('fetchThread', { id: this.id }).then(thread => {
           // fetch user
-          firebase.database().ref('users').child(thread.userId).once('value', snapshot => {
-            const user = snapshot.val()
-            this.$store.commit('setUser', { userId: snapshot.key, user: { ...user, '.key': snapshot.key } })
-          })
+          this.$store.dispatch('fetchUser', { id: thread.userId })
 
           Object.keys(thread.posts).forEach(postId => {
             // fetch post
-            firebase.database().ref('posts').child(postId).once('value', snapshot => {
-              const post = snapshot.val()
-              this.$store.commit('setPost', { postId: snapshot.key, post: { ...post, '.key': snapshot.key } })
-
+            this.$store.dispatch('fetchPost', { id: postId }).then(post => {
               // fetch user per each post.
-              firebase.database().ref('users').child(post.userId).once('value', snapshot => {
-                const user = snapshot.val()
-                this.$store.commit('setUser', { userId: snapshot.key, user: { ...user, '.key': snapshot.key } })
-              })
+              this.$store.dispatch('fetchUser', { id: post.userId })
             })
           })
         })
--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,6 @@
 import Vue from 'vue'
+import firebase from 'firebase/app'
+import 'firebase/database'
 import Vuex from 'vuex'
 import { countObjectProperties } from '@/utils'
 
@@ -106,6 +108,39 @@ export default new Vuex.Store({
 
     updateUser ({ commit }, user) {
       commit('setUser', { userId: user['.key'], user })
+    },
+
+    fetchThread ({ state, commit }, { id }) {
+      console.log('🔥 📃', id)
+      return new Promise((resolve, reject) => {
+        firebase.database().ref('threads').child(id).once('value', snapshot => {
+          const thread = snapshot.val()
+          commit('setThread', { threadId: snapshot.key, thread: { ...thread, '.key': snapshot.key } })
+          resolve(state.threads[id])
+        })
+      })
+    },
+
+    fetchUser ({ state, commit }, { id }) {
+      console.log('🔥 🙋‍♀️', id)
+      return new Promise((resolve, reject) => {
+        firebase.database().ref('users').child(id).once('value', snapshot => {
+          const user = snapshot.val()
+          commit('setUser', { userId: snapshot.key, user: { ...user, '.key': snapshot.key } })
+          resolve(state.users[id])
+        })
+      })
+    },
+
+    fetchPost ({ state, commit }, { id }) {
+      console.log('🔥 🗣', id)
+      return new Promise((resolve, reject) => {
+        firebase.database().ref('posts').child(id).once('value', snapshot => {
+          const post = snapshot.val()
+          commit('setPost', { postId: snapshot.key, post: { ...post, '.key': snapshot.key } })
+          resolve(state.posts[id])
+        })
+      })
     }
   },
 
