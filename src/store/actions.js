8e2b1fbbbb2bc6360706567182e0f2b2f55aba83
ajoaquincardozo <a.joaquincardozo@gmail.com>
2020-08-23 22:03:16 -0300

    [VueMasterClass]:  Vuex y actions - Creacion de threads -Se realiza impl. del guardado de thredas a la DB. Para ello fue necesario:   --Tanto el thread como el post, es necesario crearlas dentro de la misma transaccion   --firebase no permite realizar crear y actualizacion de rutas relacionadas (ancestro/hijo). Por lo tanto se procede a realiza vinculacion de thread con post dentro de la creacion del thread.(2)   --Para realizar (2), fue necesario realizar la creacion dentro del post en la misma action, asi como la obtencion del id y todas las properties relacionadas.   --Una vez realizada la transaccion completa en DB, se procede a realizar las mutaciones en el siguiente orden:      ---Thread      ---Post (ya que se agrega el post al thread)

--- src/store/actions.js
+++ src/store/actions.js
@@ -22,22 +22,36 @@ export default {
 
   createThread ({ commit, state, dispatch }, { text, title, forumId }) {
     return new Promise(function (resolve, reject) {
-      const threadId = 'greatThread' + Math.random()
+      const threadId = firebase.database().ref('threads').push().key
+      const postId = firebase.database().ref('posts').push().key
       const userId = state.authId
       const publishedAt = Math.floor(Date.now() / 1000)
-      const thread = { '.key': threadId, title, forumId, publishedAt, userId }
 
-      commit('setThread', { thread, threadId })
-      commit('appendThreadToForum', { parentId: forumId, childId: threadId })
-      commit('appendThreadToUser', { parentId: userId, childId: threadId })
+      const thread = { title, forumId, publishedAt, userId, firstPostId: postId, posts: {} }
+      thread.posts[postId] = postId
+      const post = { text, publishedAt, threadId, userId }
 
-      dispatch('createPost', { text, threadId })
-        .then(post => {
-          // Actualizamos el thread para que contenga el primer post.
-          commit('setThread', { threadId, thread: { ...thread, firstPostId: post['.key'] } })
+      const updates = {}
+      updates[`threads/${threadId}`] = thread
+      updates[`forums/${forumId}/threads/${threadId}`] = threadId
+      updates[`users/${userId}/threads/${threadId}`] = threadId
+
+      updates[`posts/${postId}`] = post
+      updates[`users/${userId}/posts/${postId}`] = postId
+      firebase.database().ref().update(updates)
+        .then(() => {
+          // update thread
+          commit('setItem', { resource: 'threads', item: thread, id: threadId })
+          commit('appendThreadToForum', { parentId: forumId, childId: threadId })
+          commit('appendThreadToUser', { parentId: userId, childId: threadId })
+
+          // update post
+          commit('setItem', { resource: 'posts', item: post, id: postId })
+          commit('appendPostToThread', { parentId: post.threadId, childId: postId })
+          commit('appendPostToUser', { parentId: post.userId, childId: postId })
+
+          resolve(state.threads[threadId])
         })
-
-      resolve(state.threads[threadId])
     })
   },
 
