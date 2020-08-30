7ebe42b911489f11deb8e0be75a1ed0dab55a469
ajoaquincardozo <a.joaquincardozo@gmail.com>
2020-08-30 20:23:31 -0300

    [VueMasterClass]: Spinner + bar progress -Se implementa AppSipnner, componente base (prefijo App) -Se agrega lib. NProgress, libreria que muestra una barra de progreso
    Ambos se mestran el contenido de la pagina no haya sido cargada.
    Se implementa en App.vue para que aplique a todas las pages.
    
    -Se quita setTimeOut, de actions.js

--- src/App.vue
+++ src/App.vue
@@ -10,18 +10,21 @@
           --v-if: Tiene mayores costos de alternado y v-show: Tiene mayores costos de renderizado inicial. 
             Preferir v-show para alternar muy seguido y v-if cuando no sea propensa a cambiar durante la ejecucion(runtime).
         -->
-      <router-view v-show="showPage" @ready="showPage = true" />
-      <div v-show="!showPage">loading...</div>
+      <router-view v-show="showPage" @ready="pageReady" />
+      <AppSpinner v-show="!showPage"/>
     </div>
   </div>
 </template>
 
 <script>
 import TheNavbar from '@/components/TheNavbar'
+import AppSpinner from '@/components/AppSpinner'
+import Nprogress from 'nprogress'
 
 export default {
   components: {
-    TheNavbar
+    TheNavbar,
+    AppSpinner
   },
 
   data () {
@@ -30,9 +33,23 @@ export default {
     }
   },
 
+  methods: {
+    pageReady () {
+      this.showPage = true
+      Nprogress.done()
+    }
+  },
+
   created () {
+    Nprogress.configure({
+      speed: 200,
+      showSpinner: false
+    })
+
+    Nprogress.start()
     this.$router.beforeEach((to, from, next) => {
       this.showPage = false
+      Nprogress.start()
       next()
     })
   }
@@ -41,5 +58,10 @@ export default {
 
 <style>
 @import 'assets/css/style.css';
+@import '~nprogress/nprogress.css';
+
+#nprogress .bar {
+  background: #57AD8D;
+}
 </style>
 
