27b56527f261f1a32f6101c9cf35bd278efb985b
ajoaquincardozo <a.joaquincardozo@gmail.com>
2021-01-11 09:04:25 -0300

    [VueMasterClass]: validate + Refactor Se aplica refactor para llevar todas las validaciones personalizadas de manera que sean reutilizables dentro de la app.

--- src/pages/PageRegister.vue
+++ src/pages/PageRegister.vue
@@ -79,9 +79,8 @@
 </template>
 
 <script>
-import firebase from 'firebase/app'
-import 'firebase/database'
-import { required, email, minLength, url, helpers as vuelidateHelpers } from 'vuelidate/lib/validators'
+import { required, email, minLength, url } from 'vuelidate/lib/validators'
+import { uniqueUsername, supportedImageFile, responseOk, uniqueEmail } from '@/utils/validators'
 
 export default {
   data () {
@@ -103,30 +102,12 @@ export default {
       },
       username: {
         required,
-        unique (value) {
-          if (!vuelidateHelpers.req(value)) {
-            return true
-          }
-
-          return new Promise((resolve, reject) => {
-            firebase.database().ref('users').orderByChild('usernameLower').equalTo(value.toLowerCase())
-              .once('value', snapshot => resolve(!snapshot.exists()))
-          })
-        }
+        unique: uniqueUsername
       },
       email: {
         required,
         email,
-        unique (value) {
-          if (!vuelidateHelpers.req(value)) {
-            return true
-          }
-
-          return new Promise((resolve, reject) => {
-            firebase.database().ref('users').orderByChild('email').equalTo(value.toLowerCase())
-              .once('value', snapshot => resolve(!snapshot.exists()))
-          })
-        }
+        unique: uniqueEmail
       },
       password: {
         required,
@@ -134,26 +115,8 @@ export default {
       },
       avatar: {
         url,
-        supportedImageFile (value) {
-          if (!vuelidateHelpers.req(value)) {
-            return true
-          }
-
-          const supported = ['jpg', 'jpeg', 'gif', 'png', 'svg']
-          const suffix = value.split('.').pop()
-          return supported.includes(suffix)
-        },
-        responseOk (value) {
-          if (!vuelidateHelpers.req(value)) {
-            return Promise.resolve(true)
-          }
-
-          return new Promise((resolve) => {
-            fetch(value)
-            .then(response => resolve(response.ok))
-            .catch(() => resolve(false))
-          })
-        }
+        supportedImageFile,
+        responseOk
       }
     }
   },
